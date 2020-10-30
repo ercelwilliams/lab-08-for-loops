8d53ff94b239c59c4b6f71a285c763481aa03c8e
Ercel Williams <ercelwilliams@gmail.com>
2020-10-30 19:29:59 -0400

    modified

---
+++ average.py
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<script type="text/javascript">
+		var userinput = int (input("numberber please ")
+		var userinput2 = int (input("numberber please ")
+		var userinput3 = int (input("numberber please ")
+		var userinput4 = int (input("numberber please ")
+		if 
+	</script>
+
+</body>
+</html>
---
+++ dictinary.py
@@ -0,0 +1,2 @@
+ myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755} 
+ 
