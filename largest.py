5ae1c7d71de520e14111c8d46df6924491b3c2b3
Ercel Williams <ercelwilliams@gmail.com>
2020-10-30 18:16:47 -0400

    largest

--- largest.py
+++ largest.py
@@ -1,13 +1,17 @@
-num1 = input("Number please...")
-num2 = input("Number please...")
-num3 = input("Number please...")
+num1 = int (input("Number please..."))
+num2 = int (input("Number please..."))
+num3 = int (input("Number please..."))
+num4 = int (input("Number please..."))
 
-if (num1 > num3 == num1 > num2)
-	print ("The largest number is" + num1)
+if (num1 > num3) and (num1 > num2) and (num1 > num4):
+	print ("The largest number is" + str(num1))
 
-else (num2 > num1 == num2>num3) 
-	print ("The largest number is" + num2)
+elif (num2 > num1) and (num2 > num3) and (num2 > num4):
+	print ("The largest number is" + str(num2))
 
-else (num3 > num1 == num3>num1) 
-	print ("The largest number is " + num3)
+elif (num3 > num1) and (num3 > num2) and (num3 > num4):
+	print ("The largest number is" + str(num3))
+
+else: (num4 > num1) and (num4 > num2) and (num4 > num3)
+print ("The largest number is " + str(num4))
 
